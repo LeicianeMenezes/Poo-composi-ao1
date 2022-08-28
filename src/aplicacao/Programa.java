456ef602f71c660acbd149689edde3dc1e10bbcf
Leiciane Menezes <ldsm.ecp19@uea.edu.br>
2022-08-28 12:39:14 -0400

    Primeiro commit

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,36 @@
+package aplicacao;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import entidades.Comentario;
+import entidades.Publicacao;
+
+public class Programa {
+    public static void main(String[] args) throws ParseException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		Comentario c1 = new Comentario("Faça boa viagem!");
+		Comentario c2 = new Comentario("Uau que incrível!");
+		Publicacao p1 = new Publicacao(
+				sdf.parse("21/06/2018 13:05:44"), 
+				"Viajando para New Zealand", 
+				"Vou visitar este país maravilhoso!", 
+				12);
+		p1.addComentario(c1);
+		p1.addComentario(c2);
+		
+		Comentario c3 = new Comentario("Boa noite");
+		Comentario c4 = new Comentario("Que a forca esteja com vocêÊ!");
+		Publicacao p2 = new Publicacao(
+				sdf.parse("28/07/2018 23:14:19"), 
+				"Boa noite, galera.", 
+				"Vejo você amanha", 
+				5);
+		p2.addComentario(c3);
+		p2.addComentario(c4);
+		
+		System.out.println(p1);
+		System.out.println(p2);
+	}
+}
